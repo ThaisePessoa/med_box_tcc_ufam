5557c62737b76dd024d64b5fe38d4eacedde85ab
ThaisePessoa <106990635+ThaisePessoa@users.noreply.github.com>
2023-10-05 13:51:47 -0400

    init project medbox

---
+++ medboxV1/app/src/main/java/com/ufam/thaise/medbox/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.ufam.thaise.medbox.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ medboxV1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "medboxV1"
+include(":app")
+ 
